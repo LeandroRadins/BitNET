f2ff8b420a26a1629d0faf16bd2b0bd62613b9fa
matusalemn <matusalemn@gmail.com>
2020-09-12 19:33:33 -0300

    cambios minimos

--- resources/views/tema/index.blade.php
+++ resources/views/tema/index.blade.php
@@ -64,7 +64,7 @@
                             </div>
                             <div class="border-right  d-flex flex-column">
                                 <div class="btn-group dropleft">
-                                    <a class="btn" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                    <a class="btn " data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                         <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-three-dots-vertical" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" d="M9.5 13a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0zm0-5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0z"/>
                                           </svg>
